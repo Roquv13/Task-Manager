cea3dba39a1846e310a28c7dd7c25c56b1d5e978
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-16 17:03:32 +0100

    Delete list function

--- tasks_list.py
+++ tasks_list.py
@@ -19,3 +19,16 @@ def select_list(lists):
     except Exception as e:
         print(f"Error: {e}")
         return None, None
+    
+def delete_list(lists):
+    try:
+        list_name = input("Enter name of list to delete or type EXIT: ")
+        if list_name.upper() == "EXIT":
+            pass
+        elif list_name not in lists:
+            raise exceptions.ExistsException("List not found.")
+        else:
+            lists.pop(list_name)
+            print(f"List '{list_name} deleted.'")
+    except Exception as e:
+        print(f"Error: {e}")
