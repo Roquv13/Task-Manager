65374c066f37ce030dcc040b3786467745005bf8
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-16 03:26:14 +0100

    Use tasks list in functions

--- task_manage.py
+++ task_manage.py
@@ -1,18 +1,16 @@
-tasks = {}
-
 def get_task():
     name = input("Enter name of task: ")
     text = input("Enter text for task: ")
     return name, text
 
-def add_task():
+def add_task(tasks):
     name, text = get_task()
     tasks[name] = text
 
-def delete_task():
+def delete_task(tasks):
     name, text = get_task()
     tasks.pop(name)
 
-def edit_task():
+def edit_task(tasks):
     name, text = get_task()
     tasks[name] = text
