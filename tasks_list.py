9a299bfd62b9b5672b1a92b8079e5c1f684abf05
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-16 16:56:21 +0100

    Function improvement

--- tasks_list.py
+++ tasks_list.py
@@ -5,5 +5,7 @@ def create_list(lists):
         list_name = input("Enter name of list: ")
         if list_name in lists:
             raise exceptions.ExistsException("List already exists.")
+        lists[list_name] = {}
+        print(f"List '{list_name}' created.")
     except Exception as e:
         print(f"Error: {e}")
