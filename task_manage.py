f8494c5003eb58a054f8c75cb4c62baa0875f165
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-16 03:19:30 +0100

    New function get_task used in add_task

--- task_manage.py
+++ task_manage.py
@@ -1,6 +1,10 @@
 tasks = {}
 
+def get_task():
+    name = input("Enter name of task: ")
+    text = input("Enter text for task: ")
+    return name, text
+
 def add_task():
-    task_name = input("Enter name of task: ")
-    task_text = input("Enter text for task: ")
-    tasks[task_name] = task_text
+    name, text = get_task()
+    tasks[name] = text
