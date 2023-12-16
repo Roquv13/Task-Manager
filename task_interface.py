0505abea18c60d5fb88b5359ee4fb7b584e202fe
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-16 14:59:41 +0100

    Add try except for task interface

--- task_interface.py
+++ task_interface.py
@@ -1,4 +1,8 @@
 import task_manage
 
-print("Task\n1. Add task\n2. Edit task\n3. Delete task\n4. Print tasks\n5. Exit")
-user_choice = int(input("Select: "))
+while True:
+    try:    
+        print("Task\n1. Add task\n2. Edit task\n3. Delete task\n4. Print tasks\n5. Exit")
+        user_choice = int(input("Select: "))
+    except Exception as e:
+        print(f"Error: {e}")
