38d0d5f9479363e3350e3c7754f04c34f3e9fb3f
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-17 20:08:44 +0100

    Working function

--- task_manage.py
+++ task_manage.py
@@ -57,3 +57,8 @@ def completed(tasks, completed_tasks):
         text = tasks.get(name)
         if name not in tasks:
             raise exceptions.ExistsException("Task does not exists.")
+        completed_tasks[name] = text
+        tasks.pop(name)
+        print("Task mark as completed")
+    except Exception as e:
+        print(f"Error: {e}")
