80b6f6279c7d2b11248a2c1dc9c5a2a9dbe7b6a7
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-17 20:28:32 +0100

    Mark as completed function

--- task.py
+++ task.py
@@ -1,10 +1,12 @@
 import task_manage
 import exceptions
 
+completed = {}
+
 def interface(name_list, selected_list):
     while True:
         try:    
-            print(f"\nTask list '{name_list}'\n1. Add task\n2. Edit task\n3. Delete task\n4. Print tasks\n5. Clear tasks\n6. Exit")
+            print(f"\nTask list '{name_list}'\n1. Add task\n2. Select Task\n3. Edit task\n4. Delete task\n5. Print tasks\n6. Clear tasks\n7. Exit")
             user_choice = int(input("Select: "))
             if user_choice < 1 or user_choice > 6:
                 raise exceptions.BadValue("Number must be in range from 1 to 6.")
@@ -14,12 +16,14 @@ def interface(name_list, selected_list):
         if user_choice == 1:
             task_manage.add(selected_list)
         elif user_choice == 2:
-            task_manage.edit(selected_list)
+            task_manage.completed(selected_list, completed)
         elif user_choice == 3:
-            task_manage.delete(selected_list)
+            task_manage.edit(selected_list)
         elif user_choice == 4:
-            task_manage.display(selected_list)
+            task_manage.delete(selected_list)
         elif user_choice == 5:
-            task_manage.clear(selected_list)
+            task_manage.display(selected_list)
         elif user_choice == 6:
+            task_manage.clear(selected_list)
+        elif user_choice == 7:
             break
