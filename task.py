8b8ce998c758a5859dc7d2aa8de5eb3dc7e09c4b
Roquv13 <czarekolszewski99@gmail.com>
2023-12-19 00:20:54 +0100

    Add some changes

--- task.py
+++ task.py
@@ -2,12 +2,10 @@ import task_manage
 import exceptions
 import task_mark
 
-completed = {}
-
 def interface(name_list, selected_list):
     while True:
         try:    
-            print(f"\nTask list '{name_list}'\n1. Add task\n2. Select Task\n3. Edit task\n4. Delete task\n5. Print tasks\n6. Clear tasks\n7. Exit")
+            print(f"\nTask list '{name_list}'\n1. Add task\n2. Select Task\n3. Edit task\n4. Delete task\n5. Print tasks\n6. Clear tasks\n7. Back")
             user_choice = int(input("Select: "))
             if user_choice < 1 or user_choice > 6:
                 raise exceptions.BadValue("Number must be in range from 1 to 6.")
@@ -18,8 +16,7 @@ def interface(name_list, selected_list):
             task_manage.add(selected_list)
         elif user_choice == 2:
             task_manage.get_tasks(selected_list)
-            name_task, selected_task = task_manage.select(selected_list)
-            task_mark.interface(name_task, selected_task)
+            task_mark.interface(selected_list)
         elif user_choice == 3:
             task_manage.edit(selected_list)
         elif user_choice == 4:
