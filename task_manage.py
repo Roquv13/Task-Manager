29fe45797f3d79de6c5cc4303ae47f83b20c8258
Roquv13 <czarekolszewski99@gmail.com>
2023-12-18 23:35:37 +0100

    Move function completed

--- task_manage.py
+++ task_manage.py
@@ -50,15 +50,3 @@ def display(tasks):
 
 def clear(tasks):
     tasks.clear()
-
-def completed(tasks, completed_tasks):
-    try:
-        name = get_name()
-        text = tasks.get(name)
-        if name not in tasks:
-            raise exceptions.ExistsException("Task does not exists.")
-        completed_tasks[name] = text
-        tasks.pop(name)
-        print("Task mark as completed")
-    except Exception as e:
-        print(f"Error: {e}")
