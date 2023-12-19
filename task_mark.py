a1152c2acaafbfac4b4f86c3c1af26692a2b92a7
Roquv13 <czarekolszewski99@gmail.com>
2023-12-19 21:22:31 +0100

    Add function for marking tasks as in progress

--- task_mark.py
+++ task_mark.py
@@ -23,21 +23,30 @@ def interface(tasks):
         elif user_choice == 5:
             break
 
-completed = {}
-not_completed = {}
+completed_list = {}
+not_completed_list = {}
+in_progress_list = {}
 
 def completed(name_task, selected_task):
     try:
-        completed[name_task] = selected_task
-        print("Task mark as completed")
-        print(completed)
+        completed_list[name_task] = selected_task
+        print("Task marked as 'completed'")
+        print(completed_list)
     except Exception as e:
         print(f"Error: {e}")
 
 def not_completed(name_task, selected_task):
     try:
-        not_completed[name_task] = selected_task
-        print("Task mark as not completed")
-        print(not_completed)
+        not_completed_list[name_task] = selected_task
+        print("Task marked as 'not completed'")
+        print(not_completed_list)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def in_progress(name_task, selected_task):
+    try:
+        in_progress_list[name_task] = selected_task
+        print("Task marked as 'in progress'")
+        print(in_progress_list)
     except Exception as e:
         print(f"Error: {e}")
