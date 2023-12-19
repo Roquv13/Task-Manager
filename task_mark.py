4193ffcabba394a90b7afc2c91a54c8d4062aee3
Roquv13 <czarekolszewski99@gmail.com>
2023-12-19 21:33:09 +0100

    Delete interface function from task_mark

--- task_mark.py
+++ task_mark.py
@@ -1,28 +1,3 @@
-import task_manage
-import exceptions
-
-def interface(tasks):
-    name_task, selected_task = task_manage.select(tasks) 
-    while True:
-        try:   
-            print(f"\nMark task '{name_task}'\n1. Completed\n2. Not completed\n3. In progress\n4. Important\n5. Back")
-            user_choice = int(input("Select: "))
-            if user_choice < 1 or user_choice > 5:
-                raise exceptions.BadValue("Number must be in range from 1 to 5.")
-        except Exception as e:
-            print(f"Error: {e}")
-
-        if user_choice == 1:
-            completed(name_task, selected_task)
-        elif user_choice == 2:
-            not_completed(name_task, selected_task)
-        elif user_choice == 3:
-            in_progress(name_task, selected_task)
-        elif user_choice == 4:
-            important(name_task, selected_task)
-        elif user_choice == 5:
-            break
-
 completed_list = {}
 not_completed_list = {}
 in_progress_list = {}
