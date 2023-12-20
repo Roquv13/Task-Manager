8395d51ec6052ee6a462d4bbaf1397d495dc142f
Roquv13 <czarekolszewski99@gmail.com>
2023-12-20 01:23:20 +0100

    Change task interface

--- task/interface.py
+++ task/interface.py
@@ -7,23 +7,23 @@ def menu(name_list, selected_list):
         try:    
             print(f"\nTask list '{name_list}'\n1. Add task\n2. Select Task\n3. Edit task\n4. Delete task\n5. Print tasks\n6. Clear tasks\n7. Back")
             user_choice = int(input("Select: "))
-            if user_choice < 1 or user_choice > 6:
+            if user_choice < 1 or user_choice > 7:
                 raise exceptions.BadValue("Number must be in range from 1 to 6.")
+            if user_choice == 1:
+                manage.add(selected_list)
+            elif user_choice == 2:
+                manage.get_tasks(selected_list)
+                if len(selected_list) != 0:
+                    interface.menu(selected_list)
+            elif user_choice == 3:
+                manage.edit(selected_list)
+            elif user_choice == 4:
+                manage.delete(selected_list)
+            elif user_choice == 5:
+                manage.display(selected_list)
+            elif user_choice == 6:
+                manage.clear(selected_list)
+            elif user_choice == 7:
+                break
         except Exception as e:
             print(f"Error: {e}")
-
-        if user_choice == 1:
-            manage.add(selected_list)
-        elif user_choice == 2:
-            manage.get_tasks(selected_list)
-            interface.menu(selected_list)
-        elif user_choice == 3:
-            manage.edit(selected_list)
-        elif user_choice == 4:
-            manage.delete(selected_list)
-        elif user_choice == 5:
-            manage.display(selected_list)
-        elif user_choice == 6:
-            manage.clear(selected_list)
-        elif user_choice == 7:
-            break
