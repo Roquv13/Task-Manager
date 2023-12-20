f4b1a5b7c36f73565e0413d0df2b10175f6d710d
Roquv13 <czarekolszewski99@gmail.com>
2023-12-20 01:24:37 +0100

    Change list interface

--- list/interface.py
+++ list/interface.py
@@ -9,18 +9,17 @@ def menu(lists):
             choice_int = int(input("Enter number: "))
             if choice_int < 1 or choice_int > 5:
                 raise exceptions.BadValue("Number must be in range 1 to 5.")
+            if choice_int == 1:
+                manage.create(lists)
+            elif choice_int == 2:
+                manage.get_lists(lists)
+                name_list, selected_list = manage.select(lists)
+                interface.menu(name_list, selected_list)
+            elif choice_int == 3:
+                manage.delete(lists)
+            elif choice_int == 4:
+                manage.get_lists(lists)
+            elif choice_int == 5:
+                break
         except Exception as e:
             print(f"Error: {e}")
-
-        if choice_int == 1:
-            manage.create(lists)
-        elif choice_int == 2:
-            manage.get_lists(lists)
-            name_list, selected_list = manage.select(lists)
-            interface.menu(name_list, selected_list)
-        elif choice_int == 3:
-            manage.delete(lists)
-        elif choice_int == 4:
-            manage.get_lists(lists)
-        elif choice_int == 5:
-            break
