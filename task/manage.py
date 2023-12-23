96058f055e7ee97a2912228893be16dff5be715b
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-23 16:10:52 +0100

    Add display remaining days for task

--- task/manage.py
+++ task/manage.py
@@ -61,7 +61,7 @@ def display(tasks):
             raise exceptions.ExistsException("Task does not exists.")
         else:
             for task, text in tasks.items():
-                print(task, "-", text, "-", manage.get(task))
+                print(task, "-", text, "-", manage.get(task), "-", date.manage.remaining_days(task))
     except Exception as e:
         print(f"Error: {e}")
 
