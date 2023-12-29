64095b7c3d81a8b631c1cd8c9d8aa362cf80db21
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-29 01:47:45 +0100

    Move list and change names of file

--- tasks_list.py
+++ tasks_list.py
@@ -1,5 +1,6 @@
 import exceptions
 import task.interface
+import tasks_list
 
 def create(lists):
     try:
@@ -43,3 +44,25 @@ def get_lists(lists):
         print("All of created lists:")
         for name_list, list in lists.items():
             print(name_list)
+
+def menu(lists):
+    while True:
+        print("\nUser Menu\n1. Create a new list\n2. Select list\n3. Delete list\n4. Names of lists\n5. Exit")
+        try:
+            choice_int = int(input("Enter number: "))
+            if choice_int < 1 or choice_int > 5:
+                raise exceptions.BadValue("Number must be in range 1 to 5.")
+            if choice_int == 1:
+                manage.create(lists)
+            elif choice_int == 2:
+                manage.get_lists(lists)
+                name_list, selected_list = manage.select(lists)
+                interface.menu(name_list, selected_list)
+            elif choice_int == 3:
+                manage.delete(lists)
+            elif choice_int == 4:
+                manage.get_lists(lists)
+            elif choice_int == 5:
+                break
+        except Exception as e:
+            print(f"Error: {e}")
