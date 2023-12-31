36a91d83a3e5d7cd83131270bdb668d7d636e4e6
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-12-31 16:18:20 +0100

    Add sort menu

--- sort_task.py
+++ sort_task.py
@@ -5,6 +5,9 @@ def name(tasks):
 def text(tasks):
     return dict(sorted(tasks.items(), key=lambda item: item[1]))
 
+def menu(tasks):
+    
+
 #Test
 tasks = {3: "bacd", 1: "abcd", 2: "cabd"}
 print (tasks)
